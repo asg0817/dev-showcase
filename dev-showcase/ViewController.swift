e974b4834182a94aeebf212829ccefe591730ea2
asg0817 <ig@igui-asg.local>
2016-02-11 15:47:27 +0900

    Initial Commit

---
+++ dev-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-showcase
+//
+//  Created by IG on 2016. 2. 11..
+//  Copyright © 2016년 ansugeun.k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
